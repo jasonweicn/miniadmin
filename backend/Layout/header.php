71342a04e074dc59ad3caaa818af7bf1170e7490
jasonweicn <jasonwei06@hotmail.com>
2022-01-26 15:02:20 +0800

    删除一些无用的代码

--- backend/Layout/header.php
+++ backend/Layout/header.php
@@ -20,25 +20,10 @@
   <div class="layui-header">
     <div class="layui-logo">MiniAdmin</div>
     <ul class="layui-nav layui-layout-left">
-      <!-- <li class="layui-nav-item"><a href="">首页</a></li> -->
-      <!-- <li class="layui-nav-item"><a href="settings">系统设置</a></li> -->
-      <!-- <li class="layui-nav-item"><a href="<?php echo $this->baseUrl() . '/adminuser/list';?>">用户</a></li> -->
-      <!--
-      <li class="layui-nav-item">
-        <a href="javascript:;">系统设置</a>
-        <dl class="layui-nav-child">
-          <dd><a href="">用户管理</a></dd>
-          <dd><a href="">角色管理</a></dd>
-        </dl>
-      </li>
-      -->
     </ul>
     <ul class="layui-nav layui-layout-right">
       <li class="layui-nav-item">
         <a href="javascript:;">
-          <!--
-          <img src="//tva1.sinaimg.cn/crop.0.0.118.118.180/5db11ff4gw1e77d3nqrv8j203b03cweg.jpg" class="layui-nav-img">
-      -->
           <?php echo $this->admin_nickname;?>
         </a>
         <dl class="layui-nav-child">
